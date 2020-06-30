323d031d1667ae030d3ce564ff2adf39af8446ab
vedantharshu <vedantharbu@gmail.com>
2020-07-01 02:21:41 +0530

    week 2 assignment

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -13,7 +13,8 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes.js');
-
+const Promotions = require('./models/promotions.js');
+const Leaders = require('./models/leaders.js');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
